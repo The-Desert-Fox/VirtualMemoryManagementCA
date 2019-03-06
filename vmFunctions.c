cf4942db036e322c2a17fe3d9f77f81a81724b51
Desert-Fox <watters.sm@gmail.com>
2019-03-06 22:37:30 +0000

    Additions to vmFunctions
    Actually added some functions. May reorganise how Table is done

--- vmFunctions.c
+++ vmFunctions.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 #include "vmFunctions.h"
+
+vmTable* createVMTable(int len){
+  vmTable new_table = malloc(sizeof(vmTable));  //
+  new_table -> pageNumberArray = malloc(sizeof(short) * length);
+  new_table -> frameNumberArray = malloc(sizeof(short) * length);
+  new_table -> length = len;
+
+  // Filling page numbers so none are NULL
+  for(int i = 0; i < length; i++){
+    new_table -> pageNumberArray[i] = 0;
+  }
+
+  return new_table;
+}
+
+void freeVMtable(vmTable** table){
+  free((*table) -> pageNumberArray);
+  free((*table) -> frameNumberArray);
+  free(*table);
+}
+
+void displayTable(vmTable viewTable){
+  printf("\n******************* START PRINT TABLE *******************\n");
+  for(int i = 0; i < (*viewTable)->length; i++){
+    printf("Page Number = %h\tFrame number = %h\n" (*viewTable)->pageNumberArray[i], (*viewTable)-> frameNumberArray[i]);
+  }
+  printf("\n******************* END PRINT TABLE *******************\n");
+}
+
+short getPageNumber(short mask, short value, short shift){
+  return((value & mask)>>shift);
+}
+short getOffset(short mask, short value){
+  return value & mask;
+}
--- vmFunctions.h
+++ vmFunctions.h
@@ -4,10 +4,15 @@
 #define vmFunctions
 
 typedef struct vmTable{
-  // Whatever I need to define the table.
+  int *pageNumberArray;
+  int *frameNumberArray;
+  int length;
 }vmTable;
 
 vmTable* createVMTable(int length);
+
+// malloc used in createVMTable. free function would be useful
+void freeVMtable(vmTable** t);
 
 void displayTable(vmTable** viewTable);
 
@@ -17,5 +22,9 @@ void displayPhysicalMemory(unsigned short int *physicalMemory[]);
 
 // Every char is one byte. Just randomly make 2048 to 20480 that way.
 void printPhysicalMemory(unsigned short int *physicalMemory[]);
+
+short getPageNumber(short mask, short value, short shift);
+
+short getOffset(short mask, short value);
 
 #endif
